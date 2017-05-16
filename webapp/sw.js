fae16a8331be38ed3a8306cfa9aabee2075eed14
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-05-16 17:55:57 +0530

    Add service worker

---
+++ webapp/sw.js
@@ -0,0 +1,49 @@
+var APP_PREFIX = 'ANSI-ART'
+var VERSION = 'version_01'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+  './',
+  './index.html',
+  './main.css',
+  './index.js',
+  './favicon.png',
+]
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request)
+    })
+  )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
