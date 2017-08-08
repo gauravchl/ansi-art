b640f2bfd6f14a6c632d84bb76c3a08654f140a9
Vlad Kozelsky <vkozelsky@yandex-team.ru>
2017-08-08 19:32:56 +0300

    Refactor service-worker

--- webapp/sw.js
+++ webapp/sw.js
@@ -1,49 +1,43 @@
-var APP_PREFIX = 'ANSI-ART'
-var VERSION = 'version_01'
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [
+const APP_PREFIX = 'ANSI-ART';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const URLS = [
   './',
   './index.html',
   './main.css',
   './index.js',
   './favicon.png',
-]
+];
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(function (request) {
-      return request || fetch(e.request)
-    })
-  )
-})
+    caches.match(e.request).then(request => request || fetch(e.request)),
+  );
+});
 
 // Cache resources
-self.addEventListener('install', function (e) {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll(URLS)
-    })
-  )
-})
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)),
+  );
+});
 
 // Delete outdated caches
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(function (keyList) {
+    caches.keys().then((keyList) => {
       // `keyList` contains all cache names under your username.github.io
       // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
+      const cacheWhitelist = keyList.filter(key => key.indexOf(APP_PREFIX));
       // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
+      cacheWhitelist.push(CACHE_NAME);
 
-      return Promise.all(keyList.map(function (key, i) {
+      return Promise.all(keyList.map((key, i) => {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
+          console.log(`deleting cache: ${keyList[i]}`);
+          return caches.delete(keyList[i]);
         }
-      }))
-    })
-  )
-})
+      }));
+    }),
+  );
+});
